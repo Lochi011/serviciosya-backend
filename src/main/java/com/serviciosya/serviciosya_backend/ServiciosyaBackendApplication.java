9ece39b8c98f3043e5c1b505cef62abf5534f767
Santiago Lozano <slozano@correo.um.edu.uy>
2024-08-12 11:22:37 -0300

    Initial commit

---
+++ src/main/java/com/serviciosya/serviciosya_backend/ServiciosyaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.serviciosya.serviciosya_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosyaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosyaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviciosya/serviciosya_backend/ServiciosyaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviciosya.serviciosya_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosyaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
