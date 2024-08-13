07799d45536c3329d232b65a4d3d596e0ea3b3dc
Santiago Lozano <slozano@correo.um.edu.uy>
2024-08-13 15:24:31 -0300

    conexion a bdatos
    df

--- src/main/java/com/serviciosya/serviciosya_backend/ServiciosyaBackendApplication.java
+++ src/main/java/com/serviciosya/serviciosya_backend/ServiciosyaBackendApplication.java
@@ -1,13 +1,19 @@
 package com.serviciosya.serviciosya_backend;
 
+import com.serviciosya.serviciosya_backend.entities.Cliente;
+import com.serviciosya.serviciosya_backend.repositories.ClienteRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class ServiciosyaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiciosyaBackendApplication.class, args);
+	
 	}
+
 
 }
