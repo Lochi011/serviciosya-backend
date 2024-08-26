62343810ee968b56a83f5ca68de9dc525959c9a7
Santiago Lozano <slozano@correo.um.edu.uy>
2024-08-26 11:08:38 -0300

    h
    h

--- src/main/java/com/serviciosya/serviciosya_backend/business/managers/UsuarioMgr.java
+++ src/main/java/com/serviciosya/serviciosya_backend/business/managers/UsuarioMgr.java
@@ -12,25 +12,25 @@ public class UsuarioMgr {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public void agregarUsuario (Usuario usuario) throws InvalidInformation, UsuarioYaExiste{
-    if (usuario.getNombre() == null || "".equals(usuario.getNombre())
-            || usuario.getApellido() == null || "".equals(usuario.getApellido())
-            || usuario.getEmail() == null || "".equals(usuario.getEmail())
-            //Validar longitud y formato
-            || usuario.getContrasena() == null || "".equals(usuario.getContrasena())
-            ) {
-        throw new InvalidInformation("Alguno de los datos ingresados no es correcto");
-    }
+    public void agregarUsuario(Usuario usuario) throws InvalidInformation, UsuarioYaExiste {
+        if (usuario.getNombre() == null || "".equals(usuario.getNombre())
+                || usuario.getApellido() == null || "".equals(usuario.getApellido())
+                || usuario.getEmail() == null || "".equals(usuario.getEmail())
+                //Validar longitud y formato
+                || usuario.getContrasena() == null || "".equals(usuario.getContrasena())
+        ) {
+            throw new InvalidInformation("Alguno de los datos ingresados no es correcto");
+        }
 
-    // Verifico si el usuario ya existe
+        // Verifico si el usuario ya existe
         if (usuarioRepository.findOneByCedula(usuario.getCedula()) != null) {
-        throw new UsuarioYaExiste("Ya existe usuario con esa cedula");
-    }
+            throw new UsuarioYaExiste("Ya existe usuario con esa cedula");
+        }
         if (usuarioRepository.findOneByEmail(usuario.getEmail()) != null) {
-        throw new UsuarioYaExiste("Ya existe usuario con ese correo");
-    }
+            throw new UsuarioYaExiste("Ya existe usuario con ese correo");
+        }
 
         usuarioRepository.save(usuario);
 
-}
+    }
 }
