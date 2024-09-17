3e77b15deed27b47e605d171f10823e5a48d3ebb
Santiago Lozano <slozano@correo.um.edu.uy>
2024-09-17 18:11:12 -0300

    Update JwtRequestFilter.java

--- src/main/java/com/serviciosya/serviciosya_backend/business/utils/JwtRequestFilter.java
+++ src/main/java/com/serviciosya/serviciosya_backend/business/utils/JwtRequestFilter.java
@@ -30,43 +30,43 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
-        final String authorizationHeader = request.getHeader("Authorization");
-
-        String username = null;
-        String jwt = null;
-
-
-        // Skip filtering for public endpoints
-        String requestURI = request.getRequestURI();
-        if (requestURI.equals("/api/login") || requestURI.equals("/api/register") || requestURI.equals("/api/solicitud-rubro/pendientes")) {
-            chain.doFilter(request, response);
-            return;
-        }
-
-
-        // Check if the Authorization header is present and starts with "Bearer "
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            // Extract the JWT from the Authorization header
-            jwt = authorizationHeader.substring(7);
-            // Extract the username from the JWT token
-            username = jwtUtils.extractUsername(jwt);
-        }
-
-        // If the username is not null and the user is not yet authenticated
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            // Load the user details from the database or other source
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-
-            // Validate the token
-            if (jwtUtils.validateToken(jwt, userDetails)) {
-                // Set up the authentication token to pass security checks
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                // Set the security context with the authenticated user
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        }
+//        final String authorizationHeader = request.getHeader("Authorization");
+//
+//        String username = null;
+//        String jwt = null;
+//
+//
+//        // Skip filtering for public endpoints
+//        String requestURI = request.getRequestURI();
+//        if (requestURI.equals("/api/login") || requestURI.equals("/api/register") || requestURI.equals("/api/solicitud-rubro/pendientes")) {
+//            chain.doFilter(request, response);
+//            return;
+//        }
+//
+//
+//        // Check if the Authorization header is present and starts with "Bearer "
+//        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+//            // Extract the JWT from the Authorization header
+//            jwt = authorizationHeader.substring(7);
+//            // Extract the username from the JWT token
+//            username = jwtUtils.extractUsername(jwt);
+//        }
+//
+//        // If the username is not null and the user is not yet authenticated
+//        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//            // Load the user details from the database or other source
+//            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+//
+//            // Validate the token
+//            if (jwtUtils.validateToken(jwt, userDetails)) {
+//                // Set up the authentication token to pass security checks
+//                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+//                        userDetails, null, userDetails.getAuthorities());
+//                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//                // Set the security context with the authenticated user
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
+//            }
+//        }
         // Continue the filter chain
         chain.doFilter(request, response);
     }
