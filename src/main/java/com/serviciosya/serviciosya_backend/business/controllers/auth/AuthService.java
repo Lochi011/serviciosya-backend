619e99f03030ec0c4265ec213df238571f449da1
Santiago Lozano <slozano@correo.um.edu.uy>
2024-10-02 12:26:16 -0300

    token quedo

--- src/main/java/com/serviciosya/serviciosya_backend/business/controllers/auth/AuthService.java
+++ src/main/java/com/serviciosya/serviciosya_backend/business/controllers/auth/AuthService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,16 @@ public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     public AuthResponse login(LoginRequest request) {
+
+        Usuario usuario = usuarioRepository.findOneByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        if (usuario.getContrasena().equals(request.getContraseña()))
+            usuario.setContrasena(passwordEncoder.encode(request.getContraseña()));
+            usuarioRepository.save(usuario);
+
+        if (request.getContraseña() == null || request.getContraseña().isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getContraseña()));
         UserDetails user = usuarioRepository.findOneByEmail(request.getEmail()).orElseThrow();
         String token = jwtService.getToken(user);
@@ -33,6 +44,9 @@ public class AuthService {
 
     }
     public AuthResponse register(RegisterRequest request) {
+        if (request.getContrasena() == null || request.getContrasena().isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
         Usuario usuario = Usuario.builder()
                 .cedula(request.getCedula())
                 .nombre(request.getNombre())
